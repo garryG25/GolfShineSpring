75ff1d85d8aa2bbd43d1376d1899d6a8370f3b11
garryG25 <151993576+garryG25@users.noreply.github.com>
2025-09-19 15:08:26 +0200

    Add files via upload

---
+++ GolfWash/GolfWash/src/main/java/com/GolfWash/GolfWash/GolfWashApplication.java
@@ -0,0 +1,13 @@
+package com.GolfWash.GolfWash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfWashApplication.class, args);
+	}
+
+}
---
+++ GolfWash/GolfWash/src/test/java/com/GolfWash/GolfWash/GolfWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GolfWash.GolfWash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
